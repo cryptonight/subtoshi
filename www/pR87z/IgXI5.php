9b4aa36c13e30df826624eec022fd5633007dbd0
Pierce Stegman <pwstegman@gmail.com>
2014-08-29 20:21:45 +0000

    Started admin dash

---
+++ www/pR87z/IgXI5.php
@@ -0,0 +1,13 @@
+<?php include($_SERVER['DOCUMENT_ROOT']."/includes/header.php"); ?>
+
+<div class="box box-main">
+
+<div class="page-header" style="border-bottom:1px solid #ddd;">
+  <h3>Admin Dash</h3>
+</div>
+
+</div>
+
+<?php
+
+include($_SERVER['DOCUMENT_ROOT']."/includes/footer.php");
